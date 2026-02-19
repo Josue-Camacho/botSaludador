cf69df4bc9303f74d1d5172205aefd2d1d7cea41
Josue-Camacho <josuecamacho582@gmail.com>
2026-02-18 22:16:36 -0400

    test: añadir pruebas para saludo según hora

--- src/saludador.test.js
+++ src/saludador.test.js
@@ -1,11 +1,17 @@
 import saludar from "./saludador";
 
-describe("Saludador básico", () => {
-  it("deberia saludar con hola si no hay nombre", () => {
-    expect(saludar()).toEqual("Hola");
+describe("Saludador con hora", () => {
+
+  it("deberia saludar con buenos dias en la mañana", () => {
+    expect(saludar("Juan", 9)).toEqual("Buenos días, Juan");
   });
 
-  it("deberia saludar con nombre si se proporciona", () => {
-    expect(saludar("Juan")).toEqual("Hola, Juan");
+  it("deberia saludar con buenas tardes en la tarde", () => {
+    expect(saludar("Juan", 15)).toEqual("Buenas tardes, Juan");
   });
+
+  it("deberia saludar con buenas noches en la noche", () => {
+    expect(saludar("Juan", 21)).toEqual("Buenas noches, Juan");
+  });
+
 });
