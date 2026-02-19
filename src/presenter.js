3fa8ed5e769fae1268038fb74420538170f6ae2a
Josue-Camacho <josuecamacho582@gmail.com>
2026-02-18 22:06:54 -0400

    feat:implementar saludo basico con hola

--- src/presenter.js
+++ src/presenter.js
@@ -1,23 +1,9 @@
-import sumar, { multiplicar } from "./sumador";
+import saludar from "./saludador";
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
-const btnMultiplicar = document.querySelector("#btn-multiplicar");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
-
-btnMultiplicar.addEventListener("click", () => {
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
-
-  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+  div.innerHTML = "<p>" + saludar() + "</p>";
 });
