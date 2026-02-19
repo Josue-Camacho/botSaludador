e878d9dd09ad7affb3655915ad09211ab9f98ef5
Josue-Camacho <josuecamacho582@gmail.com>
2026-02-18 22:12:09 -0400

    test: modificando el test para saludar con nombre

--- src/saludador.test.js
+++ src/saludador.test.js
@@ -1,7 +1,11 @@
 import saludar from "./saludador";
 
 describe("Saludador básico", () => {
-  it("deberia saludar con hola", () => {
+  it("deberia saludar con hola si no hay nombre", () => {
     expect(saludar()).toEqual("Hola");
+  });
+
+  it("deberia saludar con nombre si se proporciona", () => {
+    expect(saludar("Juan")).toEqual("Hola, Juan");
   });
 });
