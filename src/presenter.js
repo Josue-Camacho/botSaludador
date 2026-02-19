6b52c63d9b20ddee39cd380e6d39c5d8d91db3b1
Josue-Camacho <josuecamacho582@gmail.com>
2026-02-18 21:55:46 -0400

    feat: inicializar proyecto base saludador

---
+++ src/presenter.js
@@ -0,0 +1,23 @@
+import sumar, { multiplicar } from "./sumador";
+
+const first = document.querySelector("#primer-numero");
+const second = document.querySelector("#segundo-numero");
+const form = document.querySelector("#sumar-form");
+const div = document.querySelector("#resultado-div");
+const btnMultiplicar = document.querySelector("#btn-multiplicar");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+btnMultiplicar.addEventListener("click", () => {
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div.innerHTML = "<p>" + multiplicar(firstNumber, secondNumber) + "</p>";
+});
---
+++ src/sumador.js
@@ -0,0 +1,10 @@
+function sumar(a, b) {
+  return a + b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+export { multiplicar };
+export default sumar;
