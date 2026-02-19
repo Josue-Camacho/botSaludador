726749d72391c3cfd5be96e9c5e11af846fe8b96
Josue-Camacho <josuecamacho582@gmail.com>
2026-02-18 22:37:26 -0400

    feat: implementar soporte de idioma ingles

--- src/saludador.js
+++ src/saludador.js
@@ -1,23 +1,28 @@
 function obtenerSaludoPorHora(hora, idioma) {
-  if (idioma === "es") {
-    if (hora >= 0 && hora < 12) return "Buenos días";
-    if (hora >= 12 && hora < 19) return "Buenas tardes";
-    return "Buenas noches";
+  if (idioma === "en") {
+    if (hora >= 0 && hora < 12) return "Good morning";
+    if (hora >= 12 && hora < 19) return "Good afternoon";
+    return "Good evening";
   }
 
-  // fallback
+  // español por defecto
   if (hora >= 0 && hora < 12) return "Buenos días";
   if (hora >= 12 && hora < 19) return "Buenas tardes";
   return "Buenas noches";
 }
 
 function obtenerTitulo(genero, edad, idioma) {
-  if (idioma === "es") {
-    if (edad !== undefined && edad < 30) return "joven";
-    if (genero === "M") return "señor";
-    if (genero === "F") return "señora";
+  if (idioma === "en") {
+    if (edad !== undefined && edad < 30) return "young";
+    if (genero === "M") return "Mr.";
+    if (genero === "F") return "Ms.";
+    return "";
   }
 
+  // español
+  if (edad !== undefined && edad < 18) return "joven";
+  if (genero === "M") return "señor";
+  if (genero === "F") return "señora";
   return "";
 }
 
