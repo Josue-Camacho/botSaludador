3f62609895f571c25d1b715766f181daec198e18
Josue-Camacho <josuecamacho582@gmail.com>
2026-02-18 22:30:37 -0400

    feat(ui): agregar campo de edad al formulario

--- src/presenter.js
+++ src/presenter.js
@@ -2,6 +2,7 @@ import saludar from "./saludador";
 
 const input = document.querySelector("#nombre");
 const generoSelect = document.querySelector("#genero");
+const edadInput = document.querySelector("#edad");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -10,6 +11,7 @@ form.addEventListener("submit", (event) => {
 
   const nombre = input.value;
   const genero = generoSelect.value;
+  const edad = edadInput.value ? Number(edadInput.value) : undefined;
 
-  div.innerHTML = "<p>" + saludar(nombre, undefined, genero) + "</p>";
+  div.innerHTML = "<p>" + saludar(nombre, undefined, genero, edad) + "</p>";
 });
